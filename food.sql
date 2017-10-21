a54b04f4b5dd188da8f24aefa72717878f89d1d7
CetaneChris <christopher.raymond@mavs.uta.edu>
2017-10-21 18:29:37 -0500

    Changed to PHPMyAdmin dump

--- food.sql
+++ food.sql
@@ -1,31 +1,35 @@
-CREATE DATABASE  IF NOT EXISTS `food` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `food`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: food
--- ------------------------------------------------------
--- Server version	5.7.14
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2017 at 11:28 PM
+-- Server version: 5.7.14-log
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `food`
+--
+CREATE DATABASE IF NOT EXISTS `food` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `food`;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `customer`
 --
 
 DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `IDNO` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `customer` (
   `NAME` varchar(254) NOT NULL,
   `PHONENO` varchar(12) NOT NULL,
   `ADDRESS` varchar(256) NOT NULL,
@@ -33,83 +37,45 @@ CREATE TABLE `customer` (
   `USERNAME` varchar(12) NOT NULL,
   `PASSWORD` varchar(12) NOT NULL,
   `CREATEDDATE` datetime NOT NULL,
+  `IDNUMBER` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`IDNUMBER`),
   UNIQUE KEY `EMAIL_UNIQUE` (`EMAIL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `order`
 --
 
 DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `order` (
-  `PRODUCT` varchar(15) NOT NULL,
+CREATE TABLE IF NOT EXISTS `order` (
+  `PRODUCT_ID` int(11) DEFAULT NULL,
   `CUST_IDNO` varchar(45) NOT NULL,
   `QUANTITY` int(11) NOT NULL,
   `PRICE_EACH` varchar(10) NOT NULL,
   `TOTAL_PAID` varchar(10) NOT NULL,
-  PRIMARY KEY (`PRODUCT`)
+  `ORDER_NUM` int(11) NOT NULL,
+  PRIMARY KEY (`ORDER_NUM`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `product`
 --
 
 DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `product` (
-  `PRODUCTID` varchar(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `product` (
+  `PRODUCT_ID` int(11) NOT NULL AUTO_INCREMENT,
   `DESCRIPTION` varchar(254) NOT NULL,
-  `TYPE` enum('APPETIZER','SALAD','BEVERAGE','MAINDISH','DESSERT') NOT NULL,
+  `TYPE` enum('APPETIZER','SALAD','BEVERAGE','MAINDISH','DESSERT') DEFAULT NULL,
   `QUANTITY` int(11) NOT NULL,
   `COST` varchar(10) NOT NULL,
   `PRODUCT_IMAGE` varchar(254) NOT NULL,
-  PRIMARY KEY (`PRODUCTID`)
+  PRIMARY KEY (`PRODUCT_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `product`
---
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'food'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-10-05 14:29:23
